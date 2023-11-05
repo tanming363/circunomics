06e6695b131e243f88f1f0cfc6a3c7697d15e582
tanming363 <ingoletanmay195@gmail.com>
2023-11-05 23:04:15 +0100

    updated rout file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GithubRepoModalComponent } from './github-repo-modal/github-repo-modal.component';
+import { GithubListComponent } from './github-list/github-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: GithubListComponent },
+  { path: ':id', component: GithubRepoModalComponent },
+  { path: '**', component: GithubListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
